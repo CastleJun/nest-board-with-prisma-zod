f03fef1e29c0f31f26f0444ba59d8360d1c87fda
dev.jun28 <hsj9360@gmail.com>
2023-08-13 03:07:34 +0900

    feat: zod dto 사용을 위한 providers 추가 & password middleware 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,27 @@ import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './logger/logger.middleware';
 import { PrismaModule } from './prisma/prisma.module';
+import * as process from 'process';
+import { APP_PIPE } from '@nestjs/core';
+import { ZodValidationPipe } from 'nestjs-zod';
+import { ExcludePasswordMiddleware } from './prisma/prisma.middleware';
 
 @Module({
   imports: [CatsModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useClass: ZodValidationPipe,
+    },
+  ],
 })
 export class AppModule implements NestModule {
+  private readonly isDev: boolean = process.env.MODE === 'dev' ? true : false;
+
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(LoggerMiddleware).forRoutes('cats');
+    consumer.apply(ExcludePasswordMiddleware).forRoutes('*');
   }
 }
