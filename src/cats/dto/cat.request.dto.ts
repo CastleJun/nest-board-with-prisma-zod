84ac1e7f853bf52013699ec53918d36e45368017
dev.jun28 <hsj9360@gmail.com>
2023-08-17 01:29:34 +0900

    refactor: test swagger describe

--- src/cats/dto/cat.request.dto.ts
+++ src/cats/dto/cat.request.dto.ts
@@ -1,14 +1,16 @@
 import { createZodDto, zodToOpenAPI } from 'nestjs-zod';
 import { z } from 'nestjs-zod/z';
 
-const CatRequestSchema = z.object({
-  id: z.string().optional(),
-  email: z.string().email(),
-  password: z
-    .string()
-    .min(4, { message: 'password should not be less then 4' }),
-  name: z.string().min(4, { message: 'name should not be less then 4' }),
-});
+const CatRequestSchema = z
+  .object({
+    id: z.string().optional(),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(4, { message: 'password should not be less then 4' }),
+    name: z.string().min(4, { message: 'name should not be less then 4' }),
+  })
+  .describe('description for cat request schema');
 
 zodToOpenAPI(CatRequestSchema);
 
