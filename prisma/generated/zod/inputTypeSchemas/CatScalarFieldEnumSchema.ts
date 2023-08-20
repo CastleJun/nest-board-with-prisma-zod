77431bc683a612dd4a9f03cf2b232e65eec96b53
dev.jun28 <hsj9360@gmail.com>
2023-08-21 02:01:10 +0900

    refactor: 사용하지 않는 폴더 제거

--- prisma/generated/zod/inputTypeSchemas/CatScalarFieldEnumSchema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod';
-
-export const CatScalarFieldEnumSchema = z.enum(['id','email','password','name','imgUrl','createdAt','updatedAt']);
-
-export default CatScalarFieldEnumSchema;
